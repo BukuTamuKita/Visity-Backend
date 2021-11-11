43ce447cbc5ee19f710456f7c3a2e74204cd1ef2
Okyaaa <abi.mofra1@gmail.com>
2021-11-12 00:09:42 +0700

    merge to main

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -168,9 +168,6 @@ class UserController extends Controller
         $user->update(['device_token'=>$request->token]);
         return response()->json(['token saved successfully.']);
     }
-<<<<<<< HEAD
-}
-=======
 
     public function logoutToken(Request $request)
     {
@@ -179,4 +176,3 @@ class UserController extends Controller
         return response()->json(['token deleted successfully.']);
     }
 }
->>>>>>> 8ba5e4856717bbcb4db7d9b0be9cc074bb1c4f3b
